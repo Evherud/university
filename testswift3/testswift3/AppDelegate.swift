2339a29f5a080df2f4c949c65cb334a5ab4fd1f6
Evherud <66168310+Evherud@users.noreply.github.com>
2024-02-14 05:10:01 +0200

    Add files via upload

---
+++ testswift3/testswift3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testswift3
+//
+//  Created by Eugeny on 20.07.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testswift3/testswift3/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  testswift3
+//
+//  Created by Eugeny on 20.07.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
